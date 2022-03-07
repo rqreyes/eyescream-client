11b8c53339bb7f4be302046ed4073cce24b68199
Randy Reyes <rqreyes@gmail.com>
2022-03-06 23:02:40 -0800

    feat: add fetching flavor info

--- src/pages/flavor-info/FlavorInfo.tsx
+++ src/pages/flavor-info/FlavorInfo.tsx
@@ -1,13 +1,48 @@
-import { Typography } from "@mui/material";
+import { CircularProgress, Stack, Typography } from "@mui/material";
+import axios from "axios";
+import { Error } from "components/Error";
+import { FlavorItemData } from "pages/home/FlavorList";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
 export const FlavorInfo = (): JSX.Element => {
   const { id } = useParams();
+  const { data, error, isLoading } = useQuery<FlavorItemData, Error>(
+    "flavorList",
+    async (): Promise<FlavorItemData> => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_SERVER}/flavors/${id}`
+      );
+
+      return data;
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
 
   return (
     <>
+      {/* handle error */}
+      {error && <Error error={error} />}
+
       <Typography variant="h1">Flavor Information</Typography>
-      <Typography variant="h2">flavor ID: {id}</Typography>
+
+      {isLoading && (
+        <Stack alignItems="center" justifyContent="center">
+          <CircularProgress sx={{ height: 100 }} />
+        </Stack>
+      )}
+
+      {data && (
+        <Typography>
+          <strong>ID</strong>: {id}
+          <br />
+          <strong>Name</strong>: {data.name}
+          <br />
+          <strong>Ingredients</strong>: {data.ingredients}
+        </Typography>
+      )}
     </>
   );
 };
