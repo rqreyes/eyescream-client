1a2ff3fcc7c21eb27bdf4ca0785f63acd452622d
Randy Reyes <rqreyes@gmail.com>
2022-03-08 00:41:46 -0800

    feat: add token data

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,20 +1,28 @@
+import { ExpandMore as ExpandMoreIcon } from "@mui/icons-material";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
   CircularProgress,
-  Divider,
   Stack,
   Typography,
 } from "@mui/material";
 import Keycloak, { KeycloakInstance } from "keycloak-js";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+const accordionDetailsStyles = {
+  width: "auto", // typescript fix
+  "& pre": {
+    wordBreak: "break-all",
+    whiteSpace: "pre-wrap",
+  },
+};
 
 export const Login = (): JSX.Element => {
   const [keycloak, setKeycloak] = useState<null | KeycloakInstance>(null);
   const [authenticated, setAuthenticated] = useState(false);
-  const [tokenList, setTokenList] = useState({
-    accessTokenParsed: {},
-    idTokenParsed: {},
-  });
+  console.log("keycloak: ", keycloak);
 
   useEffect(() => {
     const keycloak = Keycloak({
@@ -28,14 +36,6 @@ export const Login = (): JSX.Element => {
       try {
         const authenticated = await keycloak.init({ onLoad: "login-required" });
         setAuthenticated(authenticated);
-
-        const accessTokenParsed = (await keycloak.tokenParsed) ?? {};
-        const idTokenParsed = (await keycloak.idTokenParsed) ?? {};
-
-        setTokenList({
-          accessTokenParsed,
-          idTokenParsed,
-        });
       } catch (error) {
         console.log(error);
       }
@@ -50,44 +50,42 @@ export const Login = (): JSX.Element => {
           <CircularProgress sx={{ height: 100, my: 10 }} />
         </Stack>
       ) : (
-        <Stack
-          direction="row"
-          divider={<Divider orientation="vertical" flexItem />}
-          spacing={2}
-        >
-          <Box sx={{ width: "50%" }}>
-            <Typography variant="h2">Access Token</Typography>
-            <ul>
-              {React.Children.toArray(
-                Object.entries(tokenList.accessTokenParsed).map(
-                  ([key, value]) => (
-                    <li>
-                      <strong>{key}</strong>:{" "}
-                      {typeof value === "object"
-                        ? JSON.stringify(value)
-                        : typeof value === "boolean"
-                        ? value.toString()
-                        : value}
-                    </li>
-                  )
-                )
-              )}
-            </ul>
-          </Box>
-          <Box sx={{ width: "50%" }}>
-            <Typography variant="h2">ID Token</Typography>
-            <ul>
-              {React.Children.toArray(
-                Object.entries(tokenList.idTokenParsed).map(([key, value]) => (
-                  <li>
-                    <strong>{key}</strong>:{" "}
-                    {typeof value === "boolean" ? value.toString() : value}
-                  </li>
-                ))
-              )}
-            </ul>
-          </Box>
-        </Stack>
+        <>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>ID Token Parsed</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={accordionDetailsStyles}>
+              <pre>{JSON.stringify(keycloak.idTokenParsed, null, 2)}</pre>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>Access Token Parsed</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={accordionDetailsStyles}>
+              <pre>{JSON.stringify(keycloak.tokenParsed, null, 2)}</pre>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>ID Token</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={accordionDetailsStyles}>
+              <pre>{JSON.stringify(keycloak.idToken, null, 2)}</pre>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>Access Token</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={accordionDetailsStyles}>
+              <Box sx={{ whiteSpace: "pre-wrap" }}>
+                <pre>{JSON.stringify(keycloak.token, null, 2)}</pre>
+              </Box>
+            </AccordionDetails>
+          </Accordion>
+        </>
       )}
     </>
   );
