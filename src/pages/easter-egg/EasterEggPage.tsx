a9d67410d228a19ca5b3fbf6889ae76ae84c0594
Randy Reyes <rqreyes@gmail.com>
2022-03-12 17:14:23 -0800

    refactor: create file structure for easter egg page

---
+++ src/pages/easter-egg/EasterEggPage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export const EasterEggPage = (): JSX.Element => (
+  <Typography variant="h1">Easter Egg</Typography>
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { EasterEggPage } from "./easter-egg";
+import { FlavorInfoPage } from "./flavor-info";
+import { HomePage } from "./home";
+import { NotFoundPage } from "./not-found";
+
+export const pages = [
+  {
+    element: <HomePage />,
+    path: "/",
+  },
+  {
+    element: <EasterEggPage />,
+    headerName: "Easter Egg",
+    path: "/easter-egg",
+    privateRoute: true,
+  },
+  {
+    element: <FlavorInfoPage />,
+    path: "/flavor-info/:id",
+  },
+  {
+    element: <NotFoundPage />,
+    path: "*",
+  },
+];
